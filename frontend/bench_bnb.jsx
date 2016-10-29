fa5c6116fc03e3694042aff3617ee3e0fab0676e
Melissa Lau <mlau@Melissas-MacBook-Pro.local>
2016-10-28 17:01:39 -0700

    wrap Root component in Router

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout } from './actions/session_actions.js';
 import configureStore from './store/store';
-
-window.signup = signup;
-window.login = login;
-window.logout = logout;
-const store = configureStore();
-window.store = store;
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore();
   let rootEl = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, rootEl);
+  ReactDOM.render(<Root store={store} />, rootEl);
 });
